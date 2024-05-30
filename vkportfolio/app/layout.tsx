d85e8967c75939edae0342c8c31ad5843706f2d2
vishwa <vishwavirat774@gmail.com>
2024-05-30 16:26:10 +0530

    google analytics changes

--- vkportfolio/app/layout.tsx
+++ vkportfolio/app/layout.tsx
@@ -2,7 +2,12 @@ import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/src/shared/components/themeprovider/themeprovider";
-import {decriptionContent,keywords} from "../src/shared/contents/description"
+import {
+  decriptionContent,
+  keywords,
+} from "../src/shared/contents/description";
+import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Montserrat({ subsets: ["latin"] });
 
@@ -25,9 +30,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <Providers>
-          {children}
-        </Providers>
+        <GoogleTagManager gtmId="GTM-P9TS4LC3" />
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
